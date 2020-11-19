2a45f8bc8386093620e3264c2c503062165b7b4c
Patrik Sjöberg <voxxar@gmail.com>
2020-11-19 15:16:21 +0100

    Add rendering and docs

--- poppler.lua
+++ poppler.lua
@@ -1,15 +1,21 @@
+--- Poppler pdf
+-- Requires luajit and ffi
+-- @module Poppler
 
 --- CDEF ---
-ffi = require 'ffi'
+local ffi = require 'ffi'
 ffi.cdef[[
 typedef struct {
   int       domain;
   int         code;
   char       *message;
 } GError;
+typedef void cairo_t;
 typedef char gchar;
 typedef void PopplerDocument;
 typedef void PopplerPage;
+
+
 const char *poppler_get_version(void);
 
 // document
@@ -20,9 +26,10 @@ gchar *poppler_document_get_title(PopplerDocument *document);
 
 // page
 void poppler_page_get_size(PopplerPage *page, double *width, double *height);
+void poppler_page_render(PopplerPage *page, cairo_t *cairo);
 ]]
 
---- globals --- 
+--- objects --- 
 local poppler = ffi.load("poppler-glib")
 
 --- Error ---
@@ -45,6 +52,9 @@ end
 
 Document = {}
 
+--- Open a document
+-- @tparam string uri A full format uri locating a file to open. 
+-- @raise Throws a string message on error
 function Document:open(uri)
   local err = Error:new()
   local doc = poppler.poppler_document_new_from_file(file, nil, err.err)
@@ -58,24 +68,37 @@ function Document:open(uri)
   return t
 end
 
+--- Get the document title
+-- @treturn string The title
 function Document:title()
   local title = poppler.poppler_document_get_title(self.doc)
   return ffi.string(title)
 end
 
+--- Get the number of pages in the document
+-- @treturns int The number of pages
 function Document:pageCount()
   local pagecount = poppler.poppler_document_get_n_pages(self.doc)
   return pagecount
 end
 
+--- Get a page of the document.
+-- @int index The page index. Starts at 1. Must not be larger than `Document:pageCount`.
+-- @see Document:pageCount
 function Document:getPage(index)
   page = poppler.poppler_document_get_page(self.doc, index - 1)
   return Page:new(page)
 end
 
 --- PAGE ---
+
+--- Object wriapping a Poppler page
+-- @classmod Page
 Page = {}
 
+--- Get a new Page object with from poppler page
+-- @param page A PopplerPage
+-- @see Document:getPage
 function Page:new(page)
   local t = {
     page = page
@@ -85,6 +108,8 @@ function Page:new(page)
   return t
 end
 
+--- Get the size of the page
+-- @treturn {width, height} Table with width and height
 function Page:size() 
   local width = ffi.new("double[1]")
   local height = ffi.new("double[1]")
@@ -92,12 +117,21 @@ function Page:size()
   return { width = width[0], height = height[0]}
 end
 
-file = "file:///System/Applications/Utilities/System Information.app/Contents/Resources/ProductGuides/ENERGY STAR.pdf"
+--- Render the page to a Cairo surface
+-- @param surcace A cairo surface
+function Page:renderToCairoSurface(surface)
+  poppler.poppler_page_render(self.page, surface)
+end
 
-doc = Document:open(file)
-print(doc:title())
-print("Title: " .. doc:title())
-print(doc:pageCount() .. " pages")
-page = doc:getPage(1)
-size = page:size()
-print("size: " .. size.width .. "x" .. size.height)
+
+local function example()
+  file = "file:///System/Applications/Utilities/System Information.app/Contents/Resources/ProductGuides/ENERGY STAR.pdf"
+
+  doc = Document:open(file)
+  print(doc:title())
+  print("Title: " .. doc:title())
+  print(doc:pageCount() .. " pages")
+  page = doc:getPage(1)
+  size = page:size()
+  print("size: " .. size.width .. "x" .. size.height)
+end
